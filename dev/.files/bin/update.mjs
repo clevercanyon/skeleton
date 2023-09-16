49211cf0cd00303b6d26153ab56de19165e964db
Jason Caldwell <jaswrks@o5p.me>
2023-09-16 07:57:46 -0400

    Dotfiles update. [d][robotic]

--- dev/.files/bin/update.mjs
+++ dev/.files/bin/update.mjs
@@ -64,6 +64,49 @@ class TSConfig {
 }
 
 /**
+ * Wrangler command.
+ */
+class Wrangler {
+	/**
+	 * Constructor.
+	 */
+	constructor(args) {
+		this.args = args;
+	}
+
+	/**
+	 * Runs CMD.
+	 */
+	async run() {
+		await this.update();
+
+		if (this.args.dryRun) {
+			u.log($chalk.cyanBright('Dry run. This was all a simulation.'));
+		}
+	}
+
+	/**
+	 * Runs update.
+	 */
+	async update() {
+		/**
+		 * Recompiles `./wrangler.toml` file.
+		 */
+
+		u.log($chalk.green('Updating `./wrangler.toml`.'));
+		if (!this.args.dryRun) {
+			await (await import(path.resolve(projDir, './dev/.files/bin/wrangler/index.mjs'))).default({ projDir });
+		}
+
+		/**
+		 * Signals completion with success.
+		 */
+
+		u.log(await u.finaleBox('Success', 'Wrangler config update complete.'));
+	}
+}
+
+/**
  * Dotfiles command.
  */
 class Dotfiles {
@@ -499,6 +542,31 @@ await (async () => {
 			},
 			handler: async (args) => {
 				await new TSConfig(args).run();
+			},
+		})
+		.command({
+			command: ['wrangler'],
+			describe: 'Updates project `./wrangler.toml`.',
+			builder: (yargs) => {
+				return yargs
+					.options({
+						dryRun: {
+							type: 'boolean',
+							requiresArg: false,
+							demandOption: false,
+							default: false,
+							description: 'Dry run?',
+						},
+					})
+					.check(async (/* args */) => {
+						if (!(await u.isInteractive())) {
+							throw new Error('This *must* be performed interactively.');
+						}
+						return true;
+					});
+			},
+			handler: async (args) => {
+				await new Wrangler(args).run();
 			},
 		})
 		.command({
