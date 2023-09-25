4ede3635a66fd902f77df6425d357658eac927df
Jason Caldwell <jaswrks@o5p.me>
2023-09-25 11:12:39 -0400

    Dotfiles update. [d][robotic]

--- dev/.files/bin/updater/index.mjs
+++ dev/.files/bin/updater/index.mjs
@@ -216,14 +216,14 @@ export default async ({ projDir }) => {
                     updates.$ꓺset.engines.node = []; // Initialize.
                     if (nodeVersion.previous.length) updates.$ꓺset.engines.node.push(nodeVersion.previous);
                     if (nodeVersion.current.length) updates.$ꓺset.engines.node.push(nodeVersion.current);
-                    if (nodeVersion.forwardCompat.length) updates.$ꓺset.engines.node.concat(nodeVersion.forwardCompat);
+                    if (nodeVersion.forwardCompat.length) updates.$ꓺset.engines.node = updates.$ꓺset.engines.node.concat(nodeVersion.forwardCompat);
                     updates.$ꓺset.engines.node = updates.$ꓺset.engines.node.join(' || ');
                 }
                 if (Object.hasOwn(updates.$ꓺset?.engines || {}, 'npm')) {
                     updates.$ꓺset.engines.npm = []; // Initialize.
                     if (nodeVersion.npm.previous.length) updates.$ꓺset.engines.npm.push(nodeVersion.npm.previous);
                     if (nodeVersion.npm.current.length) updates.$ꓺset.engines.npm.push(nodeVersion.npm.current);
-                    if (nodeVersion.npm.forwardCompat.length) updates.$ꓺset.engines.npm.concat(nodeVersion.npm.forwardCompat);
+                    if (nodeVersion.npm.forwardCompat.length) updates.$ꓺset.engines.npm = updates.$ꓺset.engines.npm.concat(nodeVersion.npm.forwardCompat);
                     updates.$ꓺset.engines.npm = updates.$ꓺset.engines.npm.join(' || ');
                 }
             }
