e448c8a98db17bc3f99c67853f5733165935b9e8
Jason Caldwell <jaswrks@o5p.me>
2023-09-29 05:27:51 -0400

    Dotfiles update. [d][robotic]

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,7 @@
 
 import './resources/init-env.ts';
 
-import { $preact } from '@clevercanyon/utilities';
-
-const classnames = (x: string) => x;
-
 /**
  * Exports.
  */
-export default function Comp() {
-    return (
-        <div class={$preact.classes('md-l-1 m-r-2 xyz phone:m-x-4')}>
-            <div class={classnames('md-l-1 phone:m-x-4 m-r-2 xyz')}></div>
-        </div>
-    );
-} // Nothing at this time.
+export default {}; // Nothing at this time.
