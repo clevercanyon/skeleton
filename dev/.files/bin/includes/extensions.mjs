4a8f2240bdc803562fb8598fa62ff51bbfde82d0
Jason Caldwell <jaswrks@o5p.me>
2024-01-01 18:41:33 -0500

    Dotfiles update. [d][robotic]

--- dev/.files/bin/includes/extensions.mjs
+++ dev/.files/bin/includes/extensions.mjs
@@ -79,17 +79,17 @@ const extensions = {
     /**
      * By canonical.
      */
-    byCanonical: $obj.map($path.extsByCanonical(), (exts) => dot(exts)),
+    byCanonical: $obj.map($obj.cloneDeep($path.extsByCanonical()), (exts) => dot(exts)),
 
     /**
      * By VS Code lang (camelCase, includes `codeTextual`).
      */
-    byVSCodeLang: $obj.map($path.extsByVSCodeLang({ camelCase: true, enableCodeTextual: true }), (exts) => dot(exts)),
+    byVSCodeLang: $obj.map($obj.cloneDeep($path.extsByVSCodeLang({ camelCase: true, enableCodeTextual: true })), (exts) => dot(exts)),
 
     /**
      * By dev group.
      */
-    byDevGroup: $obj.map($path.jsTSExtsByDevGroup(), (exts) => dot(exts)),
+    byDevGroup: $obj.map($obj.cloneDeep($path.jsTSExtsByDevGroup()), (exts) => dot(exts)),
 };
 
 /**
